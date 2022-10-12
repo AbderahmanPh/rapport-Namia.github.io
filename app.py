48248f8b049e858949ff8acd47560645c5fa549d
AbderahmanPh <Abderahmanah605@gmail.com>
2022-10-12 20:54:49 +0000

    launch rapport-namia

--- app.py
+++ app.py
@@ -41,6 +41,8 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css],
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"},
         ])
+
+server = app.server
 app.title = "Rapport"
 
 app.layout = html.Div([
