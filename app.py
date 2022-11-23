095f3cd73535bb354daff76ac358f2ef008c79a1
AbderahmanPh <Abderahmanah605@gmail.com>
2022-11-23 21:08:21 +0000

    add rapport-namia files

---
+++ app.py
@@ -0,0 +1,73 @@
+import dash
+import dash_bootstrap_components as dbc
+
+
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP],)
+
+server = app.server
+app.title = "Rapport"
+
+app.index_string = """<!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                .hidden {
+                opacity: 0;
+                }
+
+                .visible {
+                opacity: 1;
+                transition: opacity 2s ease-out;
+                }
+            </style>
+            
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-NK3SH0SRM6"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-NK3SH0SRM6');
+            </script>
+            {%metas%}
+            <title>{%title%}</title>
+            {%favicon%}
+            {%css%}
+         
+        </head>
+        <body>
+            <script>
+                document.body.className = 'hidden';
+            </script>
+            {%app_entry%}
+            <footer>
+                {%config%}
+                {%scripts%}
+                {%renderer%}
+            </footer>
+            <script>
+                document.body.className = 'visible';
+            </script>
+        </body>
+</html> """
+
+
+navbar = dbc.NavbarSimple(
+    [
+        dbc.Button("NAMIA", href="/", color="secondary", className="me-1"),
+        dbc.Button("ASMAC", href="/asmac", color="secondary"),
+    ],
+    brand="Rapport",
+    color="primary",
+    dark=True,
+    className="mb-2",
+)
+
+app.layout = dbc.Container(
+    [navbar, dash.page_container],
+    fluid=True,
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ automate_mep_rapp/automate_mep_rap.py
@@ -0,0 +1,90 @@
+import pandas as pd
+from openpyxl import load_workbook
+
+
+## READ FILES
+mep = pd.read_excel('MEP.xls', index_col=False)
+wb = load_workbook('rapprochement.xlsx')
+
+### OPERATIONS
+###----------------------------------------------------- MEP ----------------------------------------------------------
+## MEP
+
+## drop unuseful cols
+mep.drop(columns=['Unnamed: 1', 'Unnamed: 10'], inplace=True)
+## rename cols
+mep.rename(columns={'Date': 'DATE', 3: 'T03', 4: 'T04', 5: 'T05', 6: 'T06', 7: 'T07', 'TR1': 'PR1', 'TR2': 'PR2', 'TR3': 'PR3'},
+          inplace=True)
+
+## drop the last row
+mep.drop(mep.tail(1).index, inplace=True) # drop last n rows
+
+## fill na with 0
+mep.fillna(0, inplace=True)
+
+## convert to int
+cols = mep.columns
+mep[cols[1:]] = mep[cols[1:]].astype('int')
+## set a range date
+mep['DATE'] = pd.date_range(start="2022-07-02",end="2022-08-24").append(pd.date_range(start='2022-08-26', end='2022-09-30'))
+## REMOVE 00:00:00
+mep['DATE'] = mep['DATE'].dt.date
+
+## add TOTAL column
+mep['TOTAL'] = mep[cols[1:]].sum(axis=1)
+
+## save to new file (READY FOR THE APP)
+mep.to_excel('../assets/namia/mep_rapport.xlsx', index=False)
+
+
+### ------------------------------------------------- Rapprochement -----------------------------------------------------
+sheets = wb.sheetnames
+frames = []
+
+for sh in sheets:
+    df = pd.DataFrame(data = wb[f'{sh}'].values)
+    # reassign columns
+    df = df.rename(columns=df.iloc[0]).drop(df.index[0])
+    
+    if wb[f'{sh}'].max_column > 8:
+        ## STACK all columns to one col and groupby each one
+        df_res = df.stack().reset_index().groupby(['level_1']).sum().T
+        
+        ##  order cols
+        ordered_cols = df_res.columns[3:].append(df_res.columns[:3])
+        df_res = df_res[ordered_cols]
+        ## add 'DATE' col
+        df_res.insert(0, 'DATE', f'{sh}-2022')
+        frames.append(df_res)
+    else:
+        df = pd.DataFrame(df.sum()).T
+
+        ## add 'DATE' col
+        df.insert(0, 'DATE', f'{sh}-2022')
+        frames.append(df)
+    
+result = pd.concat(frames, ignore_index=True)
+
+## df to concat TRs with their relevant in the main df
+df_TRS = result[['DATE', 'TR1', 'TR2', 'TR3']]
+df_TRS = df_TRS.rename(columns={'TR1': 'PR1', 'TR2': 'PR2', 'TR3': 'PR3'})
+
+# drop unwanted cols
+result.drop(columns=['TR1', 'TR2', 'TR3'], inplace=True)
+
+last_frames = [result, df_TRS]
+rapprochement_df = pd.concat(last_frames, ignore_index=True)
+
+# Drop last column of a dataframe (PR2)
+rapprochement_df = rapprochement_df.iloc[: , :-1]
+
+## convert date to format: y-m-d
+rapprochement_df['DATE'] = pd.to_datetime(rapprochement_df['DATE'], dayfirst=True)
+## groupby 'DATE'
+rapprochement_df = rapprochement_df.groupby('DATE').sum().reset_index()
+
+## convert date back to string (for the search) 
+rapprochement_df['DATE'] = rapprochement_df['DATE'].astype('str')
+
+## save to new file (READY FOR THE APP)
+rapprochement_df.to_excel('../assets/namia/rapprochement_rapport.xlsx', index=False)
